d8456edf74aad060db890647df8f44c438aa85a0
mabuta <mabutamail@gmail.com>
2022-07-28 15:47:16 +0300

    init

---
+++ src/test/java/ru/zalex/SpringDemoBot/SpringDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zalex.SpringDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
